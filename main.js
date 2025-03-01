dec3dcaf51ac7a6410e07b1e019e623b46254c9c
hookah-webapp-new <hookah-webapp@mail.ru>
2025-03-01 07:55:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-function parseQuery(queryString) {
-  let query = {}
-  let pairs = (
-    queryString[0] === "?" ? queryString.substr(1) : queryString
-  ).split("&")
-  for (let i = 0; i < pairs.length; i++) {
-    let pair = pairs[i].split("=")
-    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || "")
-  }
-  return query
-}
-let userChatId
-
 document.addEventListener("DOMContentLoaded", () => {
-  let app = window.Telegram.WebApp
-  let query = app.initData
-  let user_data_str = parseQuery(query).user
-  let user_data = JSON.parse(user_data_str)
   app.disableVerticalSwipes()
   app.expand()
   app.ready()
